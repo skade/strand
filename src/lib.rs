e183e9e0cab97f64de6fde028cb5a206d6825605
Florian Gilcher <florian.gilcher@asquera.de>
2014-03-29 21:46:06 +0100

    Use new attribute syntax, remove warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,9 @@
-#[crate_type = "lib"];
-#[crate_id = "strain#0.0.1"];
-#[feature(globs,phase)];
-#[phase(syntax, link)] extern crate log;
+#![crate_type = "lib"]
+#![crate_id = "strain#0.0.1"]
+#![feature(globs,phase)]
+#![phase(syntax, link)] extern crate log;
 
 extern crate leveldb;
-
-use state::State;
 
 pub mod state;
 pub mod mutable;
